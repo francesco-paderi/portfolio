0caec6e471891b13eaefb1043cc0927b286115e3
francesco-paderi <francesco.paderi14@gmail.com>
2022-11-21 18:17:42 +0100

    Update script_portfolio.js

--- script_portfolio.js
+++ script_portfolio.js
@@ -1,13 +1,14 @@
 let scrollPage = () => {
     let arrow = document.getElementById("arrow-up");
+    let navbar = document.getElementById("navbar");
     let pos = document.documentElement.scrollTop; 
 
     if (pos > 100) {
         arrow.style.display = 'inline';
-        arrow.style.display = TransitionEvent;
+        navbar.style.boxShadow = '0px 4px 11px 0px #000000';
     } else {
         arrow.style.display = 'none';
-
+        navbar.style.boxShadow = 'none';
     }
 };
 
